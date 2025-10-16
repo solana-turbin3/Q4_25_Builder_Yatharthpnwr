b78c53813d5ee8827e02da22db069e48ba40d822
Yatharth <yatharthpawr@gmail.com>
2025-10-16 15:21:15 +0530

    Added week-wise progress.

---
+++ Week-1/solana-starter/ts/cluster1/nft_metadata.ts
@@ -0,0 +1,50 @@
+import wallet from "../cluster1/turbin3-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+
+// Create a devnet connection
+const umi = createUmi("https://api.devnet.solana.com");
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+
+umi.use(irysUploader({ address: "https://devnet.irys.xyz/" }));
+umi.use(signerIdentity(signer));
+
+(async () => {
+  try {
+    // Follow this JSON structure
+    // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+    const image =
+      "https://gateway.irys.xyz/43dDx9jzwJsia4vpP6fMbfaReDAQpuZmtJG6t7zfBghG";
+    const metadata = {
+      name: "YathRUG",
+      symbol: "YRUG",
+      description: "The official yatharth rug is here.",
+      image,
+      attributes: [
+        { trait_type: "Background", value: "Blue" },
+        { trait_type: "Rarity", value: "Common" },
+        { trait_type: "Creator", value: "Turbin3 Student" },
+      ],
+      properties: {
+        files: [
+          {
+            type: "image/png",
+            uri: image,
+          },
+        ],
+      },
+      creators: [],
+    };
+    const myUri = await umi.uploader.uploadJson(metadata);
+    console.log("Your metadata URI: ", myUri);
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
+})();
---
+++ Week-1/solana-starter/ts/cluster1/spl_metadata.ts
@@ -0,0 +1,59 @@
+import wallet from "../cluster1/turbin3-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createMetadataAccountV3,
+  CreateMetadataAccountV3InstructionAccounts,
+  CreateMetadataAccountV3InstructionArgs,
+  DataV2Args,
+} from "@metaplex-foundation/mpl-token-metadata";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  publicKey,
+} from "@metaplex-foundation/umi";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+
+// Define our Mint address
+const mint = publicKey("<mint address>");
+
+// Create a UMI connection
+const umi = createUmi("https://api.devnet.solana.com");
+const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
+
+(async () => {
+  try {
+    // Start here
+    let accounts: CreateMetadataAccountV3InstructionAccounts = {
+      mint: mint,
+      signer: signer,
+    };
+
+    let data: DataV2Args = {
+      name: "Turbine3 test Token",
+      symbol: "TBTT",
+      uri: "",
+      sellerFeeBasisPoints: 1000,
+      creators: null,
+      collections: null,
+      users: null,
+    };
+
+    let args: CreateMetadataAccountV3InstructionArgs = {
+      data,
+      isMutable: true,
+      colectionDetails: null,
+    };
+
+    let tx = createMetadataAccountV3(umi, {
+      ...accounts,
+      ...args,
+    });
+
+    let result = await tx.sendAndConfirm(umi);
+    console.log(bs58.encode(result.signature));
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
+})();
